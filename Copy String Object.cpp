db43a05384f7d9083d6dad99c534d7406dfdfcae
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-07-06 11:16:26 +0530

    To copy the value of string object.

---
+++ Copy String Object.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s1, s2;
+    cout << "Enter string s1: ";
+    getline (cin, s1);
+    s2 = s1;
+    cout << "s1 = "<< s1 << "\n";
+    cout << "s2 = "<< s2;
+
+    return 0;
+}
